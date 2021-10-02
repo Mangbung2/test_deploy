88b24308af7fc336b6e7f04b4f6fc62300a44ea8
Mangbung2 <41246591+Mangbung2@users.noreply.github.com>
2021-10-02 23:47:09 +0900

    11
    11

--- forensics_snu/myenv/Scripts/forensic_snu_blog/main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Write(models.Model):
-    title = models.CharField(max_length=20) #제목최대길이 20자
-    contents = models.TextField(max_length=500) #내용최대길이 500자
-    created_at = models.DateTimeField(auto_now=True)
